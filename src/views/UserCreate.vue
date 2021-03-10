4635869903c4bb1fe01d7b1ada7460cf2692baba
JoseElver <jearturo0@misena.edu.co>
2021-03-10 08:10:44 -0500

    expansion panel

--- src/views/UserCreate.vue
+++ src/views/UserCreate.vue
@@ -1,10 +1,16 @@
 <template>
  <v-app id="inspire">
-     <v-container>
-    <div >
-        <ejs-accordion class="acordion">
-        <e-accordionitems>
-    <e-accordionitem expanded='true' header='Información personal' content='<form @submit.prevent="onFormSubmit">
+     <v-container fluid>
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div id="app">
+
+    <v-expansion-panels focusable>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Información personal</v-expansion-panel-header>
+        <v-expansion-panel-content>
+
+          <form @submit.prevent="onFormSubmit">
                 <div class="form-group">
                     <label>Primer Nombre</label>
                     <input type="text" class="form-control" v-model="usuario.primernombre" required>
@@ -46,29 +52,78 @@
                 <div class="form-group">
                     <button class="btn btn-primary btn-block">Guardar</button>
                 </div>
-            </form>'></e-accordionitem>
-    <e-accordionitem header='Propiedades' content='The Model-View-Controller (MVC) architectural pattern separates an application into three main components: the model, the view, and the controller.'></e-accordionitem>
-    <e-accordionitem header='Vehiculo' content='JavaScript (JS) is an interpreted computer programming language.It was originally implemented as part of web browsers so that client-side scripts could interact with the user, control the browser, communicate asynchronously, and alter the document content that was displayed.'></e-accordionitem>
-  </e-accordionitems>
-</ejs-accordion>
-    </div>
+            </form>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+       <v-expansion-panels focusable>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Propiedades</v-expansion-panel-header>
+        <v-expansion-panel-content>
 
+          <form @submit.prevent="onFormSubmit">
+                <div class="form-group">
+                    <label>Primer Nombre</label>
+                    <input type="text" class="form-control" v-model="usuario.primernombre" required>
+                </div>
+                <div class="form-group">
+                    <label>Segundo Nombre</label>
+                    <input type="text" class="form-control" v-model="usuario.segundonombre" required>
+                </div>
+                <div class="form-group">
+                    <label>Primer Apellido</label>
+                    <input type="text" class="form-control" v-model="usuario.primerapellido" required>
+                </div>
+                 <div class="form-group">
+                    <label>Segundo Apellido</label>
+                    <input type="text" class="form-control" v-model="usuario.segundoapellido" required>
+                </div>
+                <label>Tipo de documento</label>
+              <v-select
+      v-model="usuario.tipodocumento" 
+      :items="tipos"
+      required
+      dense
+      solo
+    ></v-select>
+             <div class="form-group">
+                    <label>Documento</label>
+                    <input type="number" class="form-control" v-model="usuario.documento" required>
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" v-model="usuario.email" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Teléfono</label>
+                    <input type="text" class="form-control" v-model="usuario.telefono" required>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block">Guardar</button>
+                </div>
+            </form>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+</div>
+        </div>
+    </div>
      </v-container>
  </v-app>
 </template>
 
 <script>
     import { db } from '../firebaseDb';
-    
-    export default {
 
+    export default {
         data() {
             return {
                 tipos: ['Cédula', 'Tarjeta de identidad'],
                 usuario: {
                     
-                },
-                 
+                }
             }
         },
         methods: {
@@ -91,12 +146,3 @@
         }
     }
 </script>
-
-<style>
-.acordion{
-  margin: 20px;
-  padding: 15px;
-}
- @import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
- @import "../../node_modules/@syncfusion/ej2-vue-navigations/styles/material.css";
-</style>
